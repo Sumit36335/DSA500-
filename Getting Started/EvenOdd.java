2c5016a8618cee2ca0f36715af0e1ea7e9b1f8b3
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-09-02 22:18:26 +0530

    Create EvenOdd.java

---
+++ Getting Started/EvenOdd.java
@@ -0,0 +1,12 @@
+ public void evenOdd(int a, int b){
+         if(a%2==0&&b%2!=0)
+        {
+            System.out.println(a);
+            System.out.println(b);
+        }
+        else
+        {
+            System.out.println(b);
+            System.out.println(a);
+        }
+    }
