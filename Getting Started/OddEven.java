6ad590517486dd7df2e23023348df96abe988c07
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-09-02 22:19:22 +0530

    Create OddEven.java

---
+++ Getting Started/OddEven.java
@@ -0,0 +1,13 @@
+static String oddEven(int N){
+        // code here
+        String str1="even";
+        String str2="odd";
+        if(N%2==0)
+        {
+            return str1;
+        }
+        else
+        {
+            return str2;
+        }
+    }
