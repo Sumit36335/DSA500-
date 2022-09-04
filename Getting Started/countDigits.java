4ee257a94890523d94cd432c897cefd3e21be136
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-09-04 09:58:23 +0530

    Create countDigits.java

---
+++ Getting Started/countDigits.java
@@ -0,0 +1,15 @@
+static int evenlyDivides(int N){
+        // code here
+        int count=0;
+        int num =N;
+        while(num>0)
+        {
+            int rem = num%10;
+            if(rem!=0 && N%rem==0)
+            {
+                count++;
+            }
+            num=num/10;
+        }
+        return count;
+    }
