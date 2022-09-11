5c2a005a8ee0dbf16da6f5a55a61fdb4096a9072
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-09-11 10:07:31 +0530

    Create Search Element in Array.java

---
+++ Arrays/Search Element in Array.java
@@ -0,0 +1,15 @@
+static int search(int arr[], int N, int X)
+    {
+        
+        // Your code here
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==X)
+            {
+                return i;
+            }
+        
+        }
+        return -1;
+        
+    }
