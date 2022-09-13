0645c5f4c22bee85cc0ada373495f8e96bb2909f
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-09-13 23:43:45 +0530

    Create Sumof2NumberinArray.java

---
+++ Getting Started/Arrays/Sumof2NumberinArray.java
@@ -0,0 +1,46 @@
+int [] sum(int[] a ,int[] b)
+    {
+        int size = (a.length>b.length)?a.length+1 :b.length+1;
+        int [] res=new int[size];
+        int i = a.length-1;
+        int j=  b.length-1;
+        int k = size-1;
+        int carry =0; 
+        while(i>=0 || j>=0 || carry >0)
+        {
+            int d1= (i>=0)? a[i]:0;
+            int d2= (j>=0)? b[j]:0;
+            if(d1+d2+carry<10)
+            {
+                res[k]=d1+d2+carry;
+                carry=0;
+            }
+            else
+            {
+                res[k]=(d1+d2+carry)%10;
+                carry=(d1+d2+carry)/10;
+            }
+            i--;
+            j--;
+            k--;
+        }
+        return res;
+        
+    }
+    ArrayList<Integer> findSum(int a[], int b[]) {
+        // code here
+        int[] res= sum(a,b);
+        ArrayList<Integer> list= new ArrayList<>();
+        for(int i =0;i<res.length;i++)
+        {
+            if(i==0 && res[i]== 0)
+            {
+                continue;
+            }
+            else
+            {
+                list.add(res[i]);
+            }
+        }
+        return list;
+    }
