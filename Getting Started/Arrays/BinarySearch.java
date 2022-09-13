e4e25ce0952f200a7922dad9250ae4c019662153
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-09-13 23:45:35 +0530

    Create BinarySearch.java

---
+++ Getting Started/Arrays/BinarySearch.java
@@ -0,0 +1,21 @@
+public int search(int[]arr, int target) {
+        int low=0;
+        int high= arr.length-1;
+        while(low<=high)
+        {
+            int mid= (low)+(high-low)/2;
+            if(arr[mid]>target)
+            {
+                high= mid-1;
+            }
+            else if(arr[mid]<target)
+            {
+                low=mid+1;
+            }
+            else if(arr[mid]== target)
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
