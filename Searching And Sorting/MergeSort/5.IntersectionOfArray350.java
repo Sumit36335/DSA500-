696334ac6644b93997b6fe2e80177537b743697e
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-11-03 15:26:34 +0530

    Create 5.IntersectionOfArray350.java

---
+++ Searching And Sorting/MergeSort/5.IntersectionOfArray350.java
@@ -0,0 +1,46 @@
+public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer>contain= new ArrayList<>();
+        int ptr1=0;
+        int ptr2=0;
+        
+        while(ptr1<nums1.length && ptr2<nums2.length)
+        {
+            if(nums1[ptr1]<nums2[ptr2])
+            {
+                ptr1++;
+            }
+            else if(nums1[ptr1]> nums2[ptr2])
+            {
+                ptr2++;
+            }
+            else
+            {
+                int val= nums1[ptr1];
+        
+                while((ptr1<nums1.length && nums1[ptr1]== val)&&(ptr2<nums2.length && nums2[ptr2]== val))
+                {
+                    contain.add(val);
+                    ptr1++;
+                    ptr2++;
+                }
+                while(ptr1<nums1.length && nums1[ptr1]== val)
+                {
+                    ptr1++;
+                }
+                
+                while(ptr2<nums2.length && nums2[ptr2]== val)
+                {
+                    ptr2++;
+                }
+            }            
+        }
+        int[] res= new int[contain.size()];
+            for(int i=0;i<res.length;i++)
+            {
+                res[i] = contain.get(i);
+            }
+        return res;
+   
+    }
