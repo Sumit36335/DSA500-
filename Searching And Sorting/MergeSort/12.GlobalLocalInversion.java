8fb9b15a5b5646d487457c17eaf3c63cf1f1bea0
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-11-03 15:34:03 +0530

    Create 12.GlobalLocalInversion.java

---
+++ Searching And Sorting/MergeSort/12.GlobalLocalInversion.java
@@ -0,0 +1,65 @@
+public static int  merge(int[] arr, int left,int mid ,int right)
+    {
+        int ptr1=left;
+        int ptr2=mid+1;
+        int ptr3=0;
+        int inv_count=0;
+        int[] res = new int[right-left+1];
+        while(ptr1<=mid && ptr2<=right)
+        {
+            if(arr[ptr1]<=arr[ptr2])
+            {
+                res[ptr3]=arr[ptr1];
+                ptr1++;
+                ptr3++;
+            }
+            else
+            {
+                inv_count+=(mid-ptr1)+1;
+                res[ptr3]=arr[ptr2];
+                ptr2++;
+                ptr3++;
+            }
+        }
+        while(ptr1<=mid)
+        {
+                res[ptr3]=arr[ptr1];
+                ptr1++;
+                ptr3++;
+        }
+        while(ptr2<=right)
+        {
+                res[ptr3]=arr[ptr2];
+                ptr2++;
+                ptr3++;
+        }
+        for(int i=left;i<=right;i++)
+        {
+            arr[i]=res[i-left];
+        }
+        return inv_count;
+    }
+    static int mergeSort(int[] arr, int l,int r)
+    {
+        if(l==r)return 0;
+        int  mid= (l+r)/2;
+        int left=mergeSort(arr,l,mid);
+        int right= mergeSort(arr,mid+1,r);
+        int curr=merge(arr,l,mid,r);
+        
+        return left+right+curr;
+    }
+    public boolean isIdealPermutation(int[] nums) {
+        
+     int Lcount=0;
+     for(int i=0;i<nums.length-1;i++)
+     {
+        if(nums[i]>nums[i+1])
+        {
+            Lcount++;
+        }
+     }
+     int Gcount=mergeSort(nums,0,nums.length-1);
+     
+     return Gcount==Lcount;
+    }
