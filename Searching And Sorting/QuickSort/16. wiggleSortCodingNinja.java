9c3d7c1f272b6dc982cebb71b843e4deaafe5aae
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-11-03 14:57:58 +0530

    Create 16. wiggleSortCodingNinja.java

---
+++ Searching And Sorting/QuickSort/16. wiggleSortCodingNinja.java
@@ -0,0 +1,21 @@
+public static void swap(int[]arr,int i, int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+	public static int[] wiggleSort(int n, int[] arr) {
+		// Write your code here.
+        for(int i=0;i<n;i=i+2)
+        {
+            if(i-1>=0 && arr[i-1]<arr[i])
+            {
+                swap(arr,i,i-1);
+            }
+            if(i+1<n && arr[i+1]<arr[i])
+            {
+                swap(arr,i,i+1);
+            }
+        }
+        return arr;
+	}
