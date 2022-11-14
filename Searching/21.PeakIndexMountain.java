1a29ad74b490d658b0aa6fdbb6344b201c4da5b1
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-11-14 22:25:23 +0530

    Create 21.PeakIndexMountain.java

---
+++ Searching/21.PeakIndexMountain.java
@@ -0,0 +1,20 @@
+ public int peakIndexInMountainArray(int[] arr) {
+        int left=1;// out of bound se bachne 
+        int right=arr.length-2;// out of bound se bachne 
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1])
+            {
+                return mid;
+            }
+            else if(arr[mid]<arr[mid+1])
+            {
+                left= mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return -1;
+    }
