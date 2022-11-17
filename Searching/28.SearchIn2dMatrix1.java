b496c58c83a72002335f597ef21febb47804f737
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-11-17 22:33:29 +0530

    Create 28.SearchIn2dMatrix1.java

---
+++ Searching/28.SearchIn2dMatrix1.java
@@ -0,0 +1,17 @@
+public boolean searchMatrix(int[][] matrix, int target) {
+        // visualizing 2 d array as  1d array
+        
+        int low=0;
+        int high= matrix.length*matrix[0].length-1;
+        
+        while(low<=high)
+        {
+            int mid= low+(high-low)/2;
+            int row=mid/matrix[0].length;
+            int col=mid%matrix[0].length;
+            if(matrix[row][col]== target) return true;
+            else if(matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return false;
+    }
