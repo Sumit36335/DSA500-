32d8c7fb3900d5aa2576c575b9f9b92d5c367a49
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-12-18 01:29:30 +0530

    Create 6.DeleteNodeWithoutHead.java

---
+++ LinkList/6.DeleteNodeWithoutHead.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void deleteNode(ListNode node) {
+        int temp= node.val;
+        node.val=node.next.val;
+        node.next.val= temp;
+
+        node.next=node.next.next;
+
+    }
+}
