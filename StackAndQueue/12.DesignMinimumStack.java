44a319aee9528753dd7efb3957fa45e909c02b98
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-12-31 23:18:54 +0530

    Update 12.DesignMinimumStack.java

--- StackAndQueue/12.DesignMinimumStack.java
+++ StackAndQueue/12.DesignMinimumStack.java
@@ -1,3 +1,6 @@
+// 1. approach
+
+
 class MinStack {
 
     Stack<Integer>min,stk;
@@ -24,5 +27,42 @@ class MinStack {
     
     public int getMin() {
         return min.peek();
+    }
+}
+
+
+// 2. approach
+
+
+class MinStack {
+
+    Stack<Long>stk= new Stack<>();
+    long min=Integer.MAX_VALUE;
+
+    public void push(int val) {
+        if(val>=min){
+            stk.push(1l*val);
+        }
+        else
+        {
+            stk.push(2l*val - min);
+            min=val;
+        }
+    }
+    
+    public void pop() { 
+        long top=stk.pop();
+        if(top>=min)return;
+        min=2l*min-top;
+    }
+    
+    public int top() {
+        long top=stk.peek();
+        if(top<min)return (int)min;
+        return (int)top;
+    }
+    
+    public int getMin() {
+        return (int)min;
     }
 }
