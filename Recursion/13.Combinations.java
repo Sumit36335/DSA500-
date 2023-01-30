5b0c9e6807ca355f90c6e248c4f49d219b2a648f
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2023-01-30 12:45:57 +0530

    Create 13.Combinations.java

---
+++ Recursion/13.Combinations.java
@@ -0,0 +1,24 @@
+ List<List<Integer>> res = new ArrayList<>();
+    public void combine(int n,int k,List<Integer>output)
+    {
+
+        if(k==0)
+        {
+            res.add(new ArrayList<>(output));
+            return;
+        }
+        if(n==0)return;
+
+
+        // yes 
+        output.add(n);
+        combine(n-1,k-1,output);
+        output.remove(output.size()-1);
+
+        //no
+        combine(n-1,k,output);
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        combine(n,k,new ArrayList<>());
+        return res;
+    }
