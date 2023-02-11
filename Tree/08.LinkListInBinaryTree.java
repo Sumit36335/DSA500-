fc539c70f874b35986618adf9787025d1db1f193
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2023-02-11 11:38:18 +0530

    Create 08.LinkListInBinaryTree.java

---
+++ Tree/08.LinkListInBinaryTree.java
@@ -0,0 +1,14 @@
+ public boolean dfs(ListNode head, TreeNode root)
+    {
+        if(head==null)return true;
+        if(root==null) return false;
+
+        if(head.val!=root.val)return false;
+        return dfs(head.next,root.left)||dfs(head.next,root.right);
+    }
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(head==null)return true;
+        if(root==null)return false;
+        return dfs(head,root)|| isSubPath(head,root.left)||
+         isSubPath(head,root.right);
+    }
