4b668a9f835135006124d0d4f9c0525470bcdfc7
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2023-02-19 12:43:13 +0530

    Create 29.levelOrderTraversal2.java

---
+++ Tree/29.levelOrderTraversal2.java
@@ -0,0 +1,25 @@
+  public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<>();
+
+        if(root==null)return levels;
+
+        Queue<TreeNode> q= new ArrayDeque<>();
+        q.add(root);
+
+        while(q.size()>0)
+        {
+            List<Integer>level = new ArrayList<>();
+
+            for(int c=q.size();c>0;c--)
+            {
+                root=q.remove();
+                level.add(root.val);
+
+                if(root.left!=null)q.add(root.left);
+                if(root.right!=null)q.add(root.right);
+            }
+            levels.add(level);
+        }
+        Collections.reverse(levels);
+        return levels;
+    }
