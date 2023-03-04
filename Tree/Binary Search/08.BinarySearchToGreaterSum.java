a3a6a01c5f777fd61d8d09628d37f81d1b2db8c2
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2023-03-04 11:15:45 +0530

    Create 08.BinarySearchToGreaterSum.java

---
+++ Tree/Binary Search/08.BinarySearchToGreaterSum.java
@@ -0,0 +1,13 @@
+int sum=0;
+    public void dfs(TreeNode root)
+    {
+        if(root==null)return ;
+        dfs(root.right);
+        sum+=root.val;
+        root.val=sum;
+        dfs(root.left);
+    }
+    public TreeNode bstToGst(TreeNode root) {
+        dfs(root);
+        return root;
+    }
