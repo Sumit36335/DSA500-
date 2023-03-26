ba08abcdb15d079cf6aeae6f7f9132d28b2e9e76
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2023-03-26 13:43:47 +0530

    Create 05.MissingNumber.java

---
+++ Hashmap/05.MissingNumber.java
@@ -0,0 +1,14 @@
+  public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        for(int i=0;i<n;i++)
+        {
+            int val= nums[i]%(n+1);
+            if(val<n)nums[val]+=(n+1);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]<=n)return i;
+        }
+        return n;
+    }
