50d89b094bc42a9b92447175a736ab572246bfc9
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2023-03-28 22:43:23 +0530

    Create 06.FindAllDisappeared.java

---
+++ Hashmap/06.FindAllDisappeared.java
@@ -0,0 +1,15 @@
+ public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n= nums.length;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            int val = (nums[i]%(n+1));
+            nums[val-1]+=(n+1);
+        }
+        List<Integer>missing= new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<=n)missing.add(i+1);
+        }
+        return missing;
+    }
